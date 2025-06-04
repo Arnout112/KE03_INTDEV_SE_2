588d5440009744db0d9278f41ae9be7e7851ad27
Arnout112 <arnout.r.t.smit@gmail.com>
2025-06-04 22:37:04 +0200

    Add product filters

--- KE03_INTDEV_SE_2_Base/Controllers/ProductsController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using DataAccessLayer;
+﻿using DataAccessLayer;
 using DataAccessLayer.Models;
+using KE03_INTDEV_SE_2_Base.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
@@ -15,29 +17,77 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
             _context = context;
         }
 
-        //// GET: Products
-        //public async Task<IActionResult> Index()
-        //{
-        //    return View(await _context.Products.ToListAsync());
-        //}
-
         // GET: Products
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(bool isOnSale, string productPart, string searchString, decimal? minPrice, decimal? maxPrice, DateTime? startDate, DateTime? endDate)
         {
             if (_context.Products == null)
             {
                 return Problem("Entity set 'MatrixIncDbContext.Products'  is null.");
             }
 
+            // Use LINQ to get list of genres.
+            IQueryable<string> partsQuery = from p in _context.Parts
+                                            orderby p.Name
+                                            select p.Name;
+
             var products = from m in _context.Products
                            select m;
 
             if (!String.IsNullOrEmpty(searchString))
             {
-                products = products.Where(s => s.Name!.ToUpper().Contains(searchString.ToUpper()));
+                // select products with matching name or description
+                products = products.Where(s =>
+                    (s.Name != null && s.Name.ToUpper().Contains(searchString.ToUpper())) ||
+                    (s.Description != null && s.Description.ToUpper().Contains(searchString.ToUpper()))
+                );
             }
 
-            return View(await products.ToListAsync());
+            if (!String.IsNullOrEmpty(productPart))
+            {
+                // select products that contain the specified part
+                products = products.Where(x => x.Parts.Any(p => p.Name == productPart));
+            }
+
+            if (isOnSale)
+            {
+                // select products that are currently on sale
+                products = products.Where(p => p.SalePrice.HasValue && p.SaleStartDate <= DateTime.UtcNow && p.SaleEndDate >= DateTime.UtcNow);
+            }
+
+            if (minPrice.HasValue)
+            {
+                products = products.Where(p => p.Price >= minPrice.Value);
+            }
+
+            if (maxPrice.HasValue)
+            {
+                products = products.Where(p => p.Price <= maxPrice.Value);
+            }
+
+            if (startDate.HasValue)
+            {
+                products = products.Where(p => p.CreatedAt >= startDate.Value);
+            }
+
+            if (endDate.HasValue)
+            {
+                products = products.Where(p => p.CreatedAt <= endDate.Value);
+            }
+
+
+            var productViewModel = new ProductViewModel
+            {
+                Products = await products.ToListAsync(),
+                Parts = new SelectList(await partsQuery.Distinct().ToListAsync()),
+                MinPrice = minPrice,
+                MaxPrice = maxPrice,
+                ProductPart = productPart,
+                SearchString = searchString,
+                EndDate = endDate,
+                StartDate = startDate,
+            };
+
+            return View(productViewModel);
         }
 
         // GET: Products/Details/5
